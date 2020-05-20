0820d656ebe4036743c731be4534d42119b4de6f
ferdia <ferdia@omko.co>
2020-05-20 19:55:56 +0100

    added placeholder code for suggest.py

---
+++ what_to_watch/suggest.py
@@ -0,0 +1,22 @@
+"""
+Suggests a movie to the user for
+"""
+
+
+def suggest_movie() -> str:
+    '''
+    Returns a movie title as a suggestion
+
+    Args
+    ---
+    None
+
+    Returns
+    ---
+    str : suggestion
+    '''
+    return "Space Jam"
+
+
+if __name__ == "__main__":
+    suggest_movie()
